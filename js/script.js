85ff106f8353072d57b3bd502a94139e67f3c2d6
tariqul-sagar <tariqulisagar@gmail.com>
2026-08-12 21:45:05 +0600

    Life and score connected with function

--- js/script.js
+++ js/script.js
@@ -8,40 +8,58 @@
 //     playGround.classList.remove('hidden');
 // }
 
+
 function handleKeyboardKeyUpEvent(event) {
-  const playerPressed = event.key;
+    const playerPressed = event.key;
 
-  // get the expected to press
-  const currentAlphabetElement = document.getElementById("current-alphabet");
-  const currentAlphabet = currentAlphabetElement.innerText;
-  const expectedAlphabet = currentAlphabet.toLowerCase();
+    // get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
 
-  // Checked matched or not
+    // Checked matched or not
 
-  if (playerPressed === expectedAlphabet) {
-    console.log("You got a point");
-    removeBgColor(expectedAlphabet);
-    continueGame();
-  } else {
-    console.log("You loss a life");
-  }
+    if (playerPressed === expectedAlphabet) {
+        // console.log('You got a point');
+
+        const currentScoreElement = document.getElementById('current-score');
+        const currentScoreText = currentScoreElement.innerText;
+        const currentScore = parseInt(currentScoreText);
+        const newScore = currentScore + 1;
+        currentScoreElement.innerText = newScore;
+
+        // start new round
+        removeBgColor(expectedAlphabet);
+        continueGame();
+    } else {
+        // console.log('You loss a life');
+        const currentLifeElement = document.getElementById('current-life');
+        const currentLifeText = currentLifeElement.innerText;
+        const currentLife = parseInt(currentLifeText);
+        const newLife = currentLife - 1;
+        currentLifeElement.innerText = newLife;
+    }
+
 }
 
-document.addEventListener("keyup", handleKeyboardKeyUpEvent);
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent)
+
 
 function continueGame() {
-  const alphabet = getARandomAlphabet();
+    const alphabet = getARandomAlphabet();
 
-  // set the random alphabet
-  const currentAlphabetElement = document.getElementById("current-alphabet");
-  currentAlphabetElement.innerText = alphabet;
+    // set the random alphabet
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
 
-  // set bg color
-  setBgColor(alphabet);
+    // set bg color
+    setBgColor(alphabet);
 }
 
+
 function play() {
-  hideElementById("home-screen");
-  showElementById("playground");
-  continueGame();
+    hideElementById('home-screen');
+    showElementById('playground');
+    continueGame();
 }
